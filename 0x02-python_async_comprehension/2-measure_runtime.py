9b69e320b72ad8300d34eab6086218daf70f4c88
kirubelks <kirubel.n.gebeyehu@gmail.com>
2023-10-10 23:41:03 +0300

    commit for all tasks of async_comprehension

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''measure Run time for four parallel comprehensions Task 2.
+'''
+import asyncio
+import time
+from importlib import import_module as using
+
+
+async_comprehension = using('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    '''Executes async_comprehension 4 times and measures the
+    total execution time.
+    '''
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    return time.time() - start_time
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,1 @@
+this is the read me file for the project 0x02-python_async_comprehension
